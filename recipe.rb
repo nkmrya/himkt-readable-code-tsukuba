9a677a970c33b019a4ffcdefdcb30cedb7359d17
himkt <himkt@klis.tsukuba.ac.jp>
2015-07-04 11:21:59 +0900

    spec6: implement

--- recipe.rb
+++ recipe.rb
@@ -11,7 +11,8 @@ def spec3
 end
 
 def spec4
-
+  
+  # レシピ
   recipes = [
     'オムライス',
     '親子丼',
@@ -24,12 +25,20 @@ def spec4
       out.puts recipe
     end
   end
-  # 登録終わり
   
   # 出力
   spec3
 end
 
+def spec5
+  id = 0
+  File.foreach('./recipe-data.txt') do |line|
+    puts "#{id}:#{line}"
+    id += 1
+  end
+end
+
+=begin
 case ARGV[0]
 when '1'
   spec1
@@ -37,7 +46,29 @@ when '3'
   spec3
 when '4'
   spec4
+when '5'
+  spec5
 else
   puts '仕様番号を指定してください'
   puts 'ruby recipe.rb <仕様番号>'
 end
+=end
+
+
+
+
+
+
+
+id = 1
+select_id = ARGV[0].to_i if ARGV[0]
+File.foreach('./recipe-data.txt') do |line|
+  puts "#{id}:#{line}" unless select_id
+  puts "#{id}:#{line}" if select_id == id
+  id+=1
+end
+
+
+
+
+
