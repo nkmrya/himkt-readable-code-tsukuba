f5de364fb9c2ea2acb8a8b4f19186db104e82580
himkt <himkt@klis.tsukuba.ac.jp>
2015-07-04 11:09:42 +0900

    spec4: imeplement

--- recipe.rb
+++ recipe.rb
@@ -10,11 +10,33 @@ def spec3
   end
 end
 
+def spec4
+
+  recipes = [
+    'オムライス',
+    '親子丼',
+    '杏仁豆腐'
+  ]
+
+  # 登録
+  open('./recipe-data.txt', 'w') do |out|
+    recipes.each do |recipe|
+      out.puts recipe
+    end
+  end
+  # 登録終わり
+  
+  # 出力
+  spec3
+end
+
 case ARGV[0]
 when '1'
   spec1
 when '3'
   spec3
+when '4'
+  spec4
 else
   puts '仕様番号を指定してください'
   puts 'ruby recipe.rb <仕様番号>'
